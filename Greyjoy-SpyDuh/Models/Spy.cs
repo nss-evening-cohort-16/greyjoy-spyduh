c4879a463f263f642e2f2f4c48ce1bd1c8d1b026
Grace Sutherland <rgs1690@gmail.com>
2022-02-14 20:40:37 -0600

    set up files

---
+++ Greyjoy-SpyDuh/Models/Spy.cs
@@ -0,0 +1,25 @@
+﻿namespace Greyjoy_SpyDuh.Models
+{
+    public class Spy
+    {
+        public List<Spy> Enemies { get; set; }
+        public List<Spy> Friends { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public List<SkillType> Skills { get; set; }
+
+        public enum SkillType
+        {
+        Sabatoge,
+        Sneaking, 
+        LockPicking,
+        Disguise,
+        Assassination,
+        Deception,
+        Seduction,
+
+
+        }
+
+    }
+}
---
+++ Greyjoy-SpyDuh/Repos/SpyRespository.cs
@@ -0,0 +1,69 @@
+﻿using Greyjoy_SpyDuh.Models;
+using static Greyjoy_SpyDuh.Models.Spy;
+
+namespace Greyjoy_SpyDuh.Repos
+{
+    public class SpyRespository
+    {
+        private static List<Spy> _spys = new List<Spy>()
+        {
+            new Spy()
+            {
+                Name = "Sterling Archer",
+                Id = 1,
+                Skills = new List<SkillType> { SkillType.Seduction, SkillType.Sabatoge, SkillType.Deception },
+                Friends = new List<Spy> {},
+                Enemies = new List<Spy> {},
+            },
+
+            new Spy()
+            {
+                Name = "Lana Kane",
+                Id = 2,
+                Skills = new List<SkillType> { SkillType.Seduction, SkillType.Disguise, SkillType.LockPicking },
+                Friends = new List<Spy> {  },
+                Enemies = new List<Spy> {},
+            },
+            new Spy()
+            {
+                Name = "Woodhouse",
+                Id = 3,
+                Skills = new List<SkillType> { SkillType.Seduction, SkillType.Sabatoge, SkillType.Assassination },
+                Friends = new List<Spy> {  },
+                Enemies = new List<Spy> {},
+            },
+            new Spy()
+            {
+                Name = "Cyril",
+                Id = 4,
+                Skills = new List<SkillType> { SkillType.Sneaking, SkillType.Disguise, SkillType.Deception, SkillType.Sabatoge },
+                Friends = new List<Spy> {  },
+                Enemies = new List<Spy> {},
+            },
+             new Spy()
+            {
+                Name = "Barry the Cyborg",
+                Id = 5,
+                Skills = new List<SkillType> { SkillType.Sneaking, SkillType.Disguise, SkillType.Deception, SkillType.Sabatoge },
+                Friends = new List<Spy> {  },
+                Enemies = new List<Spy> {},
+            },
+              new Spy()
+            {
+                Name = "Ray Gillette",
+                Id = 6,
+                Skills = new List<SkillType> { SkillType.Sneaking, SkillType.Deception, SkillType.LockPicking, SkillType.Sabatoge },
+                Friends = new List<Spy> {  },
+                Enemies = new List<Spy> {},
+            },
+               new Spy()
+            {
+                Name = "Pam Poovey",
+                Id = 7,
+                Skills = new List<SkillType> { SkillType.Sneaking, SkillType.LockPicking, SkillType.Deception, SkillType.Sabatoge },
+                Friends = new List<Spy> {  },
+                Enemies = new List<Spy> {},
+            },
+        };
+    }
+}
