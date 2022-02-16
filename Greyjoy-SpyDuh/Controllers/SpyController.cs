f7d2e8c275827114a48ab5121ca87757cf4cc101
Grace Sutherland <rgs1690@gmail.com>
2022-02-15 21:01:11 -0600

    push for austin

--- Greyjoy-SpyDuh/Controllers/SpyController.cs
+++ Greyjoy-SpyDuh/Controllers/SpyController.cs
@@ -1,6 +1,7 @@
 ﻿using Greyjoy_SpyDuh.Models;
 using Greyjoy_SpyDuh.Repos;
 using Microsoft.AspNetCore.Mvc;
+using static Greyjoy_SpyDuh.Models.Spy;
 
 namespace Greyjoy_SpyDuh.Controllers
 {
@@ -8,12 +9,82 @@ namespace Greyjoy_SpyDuh.Controllers
     [ApiController]
     public class SpyController : Controller
     {
-        SpyRespository _spyRespository = new SpyRespository();
+        SpyRepository _spyRepository = new SpyRepository();
 
         [HttpGet]
         public List<Spy> spies()
         {
-            return _spyRespository.GetAll();
+            return _spyRepository.GetAll();
+        }
+
+        [HttpPost]
+        public IActionResult PostNewSpy(Spy spy)
+        {
+            if (spy == null)
+            {
+                return NotFound();
+            }
+            else if (!ValidNewSpy(spy))
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _spyRepository.Post(spy);
+                return Ok(spy);
+            }
+        }
+
+        private bool ValidNewSpy(Spy newSpy)
+        {
+            if (_spyRepository.GetById(newSpy.Id) != null)
+            {
+                return false;
+            }
+
+            if (newSpy.Name == null)
+            {
+                return false;
+            }
+
+            if (newSpy.Friends != null)
+            {
+                foreach (var friend in newSpy.Friends)
+                {
+                    if (_spyRepository.GetById(friend) == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (newSpy.Enemies != null)
+            {
+                foreach (var enemy in newSpy.Enemies)
+                {
+                    if (_spyRepository.GetById(enemy) == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (newSpy.Skills != null)
+            {
+                foreach (var skill in newSpy.Skills)
+                {
+                    if (!Enum.IsDefined(typeof(SkillType), skill))
+                    {
+                        return false;
+                    }
+
+                }
+            }
+
+            return true;
+
         }
     }
+
+
 }
